cefa0bcfcc54bb0cd58f278687f5c1b3dd3fe405
Andrew Lodes <alodes999@gmail.com>
2015-06-09 13:25:55 -0500

    Add check_splitter_test to repository

---
+++ check_splitter_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'check_splitter.rb'
+
+class CheckSplitterTest < MiniTest::Test
+
+  # one of my specs should check to ensure tip returns a float.
+  
+  # one of my specs should check to ensure total_cost returns a float.
+  
+  # one of my specs should check to ensure split returns a float.
+  
+end
